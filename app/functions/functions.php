abe4ea19b73c7572983acc8a3339e6f9dc5e324a
Tim Brugman <tim@timbrugman.com>
2021-01-25 18:12:22 +0100

    Changed everything.

--- app/functions/functions.php
+++ app/functions/functions.php
@@ -13,74 +13,100 @@ function dd( $var )
     exit;
 }
 
+function display_as_table( $data )
+{
+    echo '<table>';
+    foreach ( $data as $row )
+    {
+        echo '<tr>';
+        foreach ( $row as $cell )
+        {
+            echo '<td>'.$cell.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+
 function convert_to_array( $data )
 {
     $data = str_getcsv( $data, "\n" );
+
     foreach ( $data as &$row )
         $row = str_getcsv( $row );
+
     return $data;
 }
 
-function get_heroes( $data )
+function get_heroes( $data, $type = false )
 {
+    if ( $type == 'damage' )
+        return array_slice( $data[1], 2, 16 );
+
+    if ( $type == 'tank' )
+        return array_slice( $data[1], 16, 8 );
+
+    if ( $type == 'support' )
+        return array_slice( $data[1], 24, 7 );
+
     return array_slice( $data[1], 2 );
 }
 
-function filter_heroes( $heroes_all, $type = false )
+function get_hero_name_by_index( $data, $hero_index )
 {
-    if ( !$type )
-        return [];
-
-    if ( $type == 'damage' )
-        return array_slice( $heroes_all, 0, 16 );
-
-    if ( $type == 'tank' )
-        return array_slice( $heroes_all, 16, 8 );
-
-    if ( $type == 'support' )
-        return array_slice( $heroes_all, 24, 7 );
+    return $data[ $hero_index ][1];
 }
 
-function get_hero_name_by_index( $hero_index )
+function heroes_strong_against( $data, $hero_index )
 {
-    global $heroes_all;
-    return $heroes_all[ $hero_index -2 ];
-}
-
-function heroes_strong_against( $hero_index )
-{
-    global $source_data;
-
     $counters = [];
 
-    $hero_row = $source_data[ $hero_index +2 ];
+    $hero_row = $data[ $hero_index +2 ];
 
     foreach ( $hero_row as $counter_index => $wl )
         if ( $wl == 'L' )
-            $counters[] = get_hero_name_by_index( $counter_index );
+            $counters[] = get_hero_name_by_index( $data, $counter_index );
 
-    return $counters;
-}
+    $hero_col = array_column( $data, $hero_index +2 );
 
-function heroes_neutral_against( $hero_index )
-{
-    global $source_data;
-
-    $counters = [];
-
-    $hero_row = $source_data[ $hero_index +2 ];
-
-    foreach ( $hero_row as $counter_index => $wl )
-        if ( $wl == 'N' )
-            $counters[] = get_hero_name_by_index( $counter_index );
+    foreach ( $hero_col as $counter_index => $wl )
+        if ( $wl == 'W' )
+            $counters[] = get_hero_name_by_index( $data, $counter_index );
 
     return $counters;
 }
 
 function slugify_name( $name )
 {
-    $name = strtolower( $name );
-    $name = str_replace( ' ', '-', $name );
-    return $name;
+    return str_replace( [' ','.'], '', strtolower( $name ) );
+}
+
+function get_source_data( $gsheet_url = false )
+{
+    if ( !$gsheet_url )
+        $gsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSecW57HwpBfbcivjEPGWkVD6iP-tDj13JbWDt1oAA6f7JYaGoRRVKswyyjPCXJ2KRITFmcvxflBa-0/pub?gid=0&single=true&output=csv';
+
+    $source_csv = @file_get_contents( $gsheet_url );
+
+    if ( !$source_csv )
+        return [ false, 'Source data could not be loaded.' ];
+
+    $source_data = convert_to_array( $source_csv );
+
+    if ( !in_array( $source_data[3][2], ['W','L'] ) )
+        return [ false, 'Google Sheets data has not properly saved.' ];
+
+    return [ true, $source_data ];
+}
+
+function pipeify( $array = [] )
+{
+    if ( empty( $array ) )
+        return '';
+
+    foreach ( $array as &$item )
+        $item = slugify_name( $item );
+
+    return implode( '|', $array );
 }
 
--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 (function($) {
 
-    console.log( 'version 1' );
-
     $('.hero').on( 'click', function ( event ) {
         // prevent default
         event.stopPropagation();
         event.preventDefault();
-        //
-        var neutralize = false;
-        if ( $(this).hasClass('active-select') ) {
-            var neutralize = true;
+        // if we neutralize
+        let neutralize = false;
+        if ( $(this).hasClass('active-select') )
+        {
+            neutralize = true;
             $('.heroes').addClass('neutral');
         }
         // remove all actives
         $('.hero').removeClass('active-select');
         $('.hero').removeClass('active-counter');
-        //
-        if ( !neutralize ) {
+        // if we dont neutralize
+        if ( !neutralize )
+        {
             $('.heroes').removeClass('neutral');
             // set this obs active
             $(this).addClass('active-select');
             // get counters
-            var is_countered_by = $(this).data('is-countered-by').split('|');
-            $( is_countered_by ).each(function(){
-                // set counters active
+            let is_countered_by = $(this).data('is-countered-by').split('|');
+            // set counters active
+            $( is_countered_by ).each( function () {
                 $( '.hero-'+this ).addClass('active-counter');
             });
         }
