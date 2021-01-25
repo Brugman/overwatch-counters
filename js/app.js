1089a123bd3ebd7a7b61bf1e6e1049563c6f25b4
Loris Bettazza <loris.bettazza@gmail.com>
2021-01-25 17:09:29 +0100

    Remove jQuery

--- js/app.js
+++ js/app.js
@@ -1,32 +1,42 @@
-(function($) {
+(function() {
 
     console.log( 'version 1' );
 
-    $('.hero').on( 'click', function ( event ) {
-        // prevent default
-        event.stopPropagation();
-        event.preventDefault();
-        //
-        var neutralize = false;
-        if ( $(this).hasClass('active-select') ) {
-            var neutralize = true;
-            $('.heroes').addClass('neutral');
-        }
-        // remove all actives
-        $('.hero').removeClass('active-select');
-        $('.hero').removeClass('active-counter');
-        //
-        if ( !neutralize ) {
-            $('.heroes').removeClass('neutral');
-            // set this obs active
-            $(this).addClass('active-select');
-            // get counters
-            var is_countered_by = $(this).data('is-countered-by').split('|');
-            $( is_countered_by ).each(function(){
-                // set counters active
-                $( '.hero-'+this ).addClass('active-counter');
-            });
-        }
+    var heroes = document.querySelector('.heroes');
+    var hero_items = document.querySelectorAll('.hero');
+
+    hero_items.forEach(function ( hero ) {
+        hero.addEventListener('click', function ( event ) {
+            // prevent default
+            event.stopPropagation();
+            event.preventDefault();
+            //
+            var this_hero = event.currentTarget;
+            var neutralize = false;
+            if ( this_hero.classList.contains('active-select') ) {
+                neutralize = true;
+                heroes.classList.add('neutral');
+            }
+            // remove all actives
+            hero_items.forEach(function (hero_item) {
+                hero_item.classList.remove('active-select', 'active-counter');
+            })
+            //
+            if ( !neutralize ) {
+                heroes.classList.remove('neutral');
+                // set this obs active
+                this_hero.classList.add('active-select');
+                // get counters
+                var is_countered_by = this_hero.dataset.isCounteredBy.split('|');
+                is_countered_by.forEach(function(counter_name){
+                    // set counters active
+                    var counter_hero = document.querySelector( '.hero-'+counter_name );
+                    if (counter_hero) {
+                        counter_hero.classList.add('active-counter');
+                    }
+                });
+            }
+        });
     });
 
-})( jQuery );
+})();
