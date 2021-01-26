881502e4c019cfdf0f837cd853fefff96487aa29
Tim Brugman <tim@timbrugman.com>
2021-01-26 13:59:53 +0100

    Minor refactoring after PR.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 (function() {
 
-    let heroes = document.querySelector('.heroes');
+    let hero_list = document.querySelector('.heroes');
     let hero_items = document.querySelectorAll('.hero');
 
-    hero_items.forEach(function ( hero ) {
-        hero.addEventListener('click', function ( event ) {
-            let this_hero = event.currentTarget;
+    hero_items.forEach( function ( hero ) {
+        hero.addEventListener( 'click', function ( event ) {
             // prevent default
             event.stopPropagation();
             event.preventDefault();
-            // if we neutralize
+            // variables
+            let this_hero = event.currentTarget;
             let neutralize = false;
-            if ( this_hero.classList.contains('active-select') )
-            {
+            // if we neutralize
+            if ( this_hero.classList.contains('active-select') ) {
                 neutralize = true;
-                heroes.classList.add('neutral');
+                // add neutral state
+                hero_list.classList.add('neutral');
             }
             // remove all actives
-            hero_items.forEach(function (hero_item) {
+            hero_items.forEach( function ( hero_item ) {
                 hero_item.classList.remove('active-select', 'active-counter');
             });
             // if we dont neutralize
-            if ( !neutralize ) 
-            {
-                heroes.classList.remove('neutral');
-                // set this obs active
+            if ( !neutralize ) {
+                // remove neutral state
+                hero_list.classList.remove('neutral');
+                // set active hero
                 this_hero.classList.add('active-select');
-                // get counters
+                // set active counters
                 let is_countered_by = this_hero.dataset.isCounteredBy.split('|');
-                // set counters active
-                is_countered_by.forEach(function(counter_name){
+                is_countered_by.forEach( function ( counter_name ) {
                     let counter_hero = document.querySelector( '.hero-'+counter_name );
-                    if (counter_hero) {
+                    if ( counter_hero ) {
                         counter_hero.classList.add('active-counter');
                     }
                 });
