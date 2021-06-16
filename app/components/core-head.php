49e2680cda7eb131435f62b5c6078468a788ccb8
Tim Brugman <tim@timbrugman.com>
2021-06-16 18:36:54 +0200

    Add favicon
    Close #13

--- app/components/core-head.php
+++ app/components/core-head.php
@@ -15,6 +15,12 @@
     <!-- link css -->
     <link rel="stylesheet" href="/assets/app.min.css">
 
+    <!-- app favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1">
+    <link rel="shortcut icon" href="/favicon.ico?v=1">
+
 </head>
 <body>
 
