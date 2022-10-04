b55456087088f83d1dd0dc151212b8261a7e6934
Tim Brugman <tim@timbrugman.com>
2022-10-05 00:15:04 +0200

    Fix indentation

--- app/components/ow1.php
+++ app/components/ow1.php
@@ -2,28 +2,28 @@
 
 <?php foreach ( ['tank','damage','support'] as $role ): ?>
 
-        <div class="role">
+    <div class="role">
 
-            <p class="title"><?=include_svg('icon-role-'.$role);?><?=ucfirst( $role );?></p>
+        <p class="title"><?=include_svg('icon-role-'.$role);?><?=ucfirst( $role );?></p>
 
-            <ul class="heroes js-heroes neutral">
+        <ul class="heroes js-heroes neutral">
 <?php
 
 foreach ( get_heroes_ow1( $data_ow1, $role ) as $hero_id => $hero_name )
 {
 ?>
-                <li class="hero js-hero js-hero-<?=slugify_name( $hero_name );?>" data-is-countered-by="<?=pipeify( heroes_strong_against( $data_ow1, $hero_id ) );?>" title="<?=$hero_name;?>">
-                    <div class="inner">
-                        <img src="/assets/images/ow1/hero-<?=slugify_name( $hero_name );?>.png" loading="lazy" alt="<?=$hero_name;?>">
-                    </div><!-- inner -->
-                </li><!-- hero -->
+            <li class="hero js-hero js-hero-<?=slugify_name( $hero_name );?>" data-is-countered-by="<?=pipeify( heroes_strong_against( $data_ow1, $hero_id ) );?>" title="<?=$hero_name;?>">
+                <div class="inner">
+                    <img src="/assets/images/ow1/hero-<?=slugify_name( $hero_name );?>.png" loading="lazy" alt="<?=$hero_name;?>">
+                </div><!-- inner -->
+            </li><!-- hero -->
 <?php
 }
 
 ?>
-            </ul><!-- heroes -->
+        </ul><!-- heroes -->
 
-        </div><!-- role -->
+    </div><!-- role -->
 
 <?php endforeach; // $roles ?>
 
--- app/components/ow2.php
+++ app/components/ow2.php
@@ -2,27 +2,27 @@
 
 <?php foreach ( ['tank','damage','support'] as $role ): ?>
 
-        <div class="role role-<?=$role;?>">
+    <div class="role role-<?=$role;?>">
 
-            <p class="title"><?=include_svg('icon-role-'.$role);?><?=ucfirst( $role );?></p>
+        <p class="title"><?=include_svg('icon-role-'.$role);?><?=ucfirst( $role );?></p>
 
-            <ul class="heroes js-heroes neutral">
+        <ul class="heroes js-heroes neutral">
 <?php
 
 foreach ( get_heroes_ow2( $data_ow2, $role ) as $hero_id => $hero_name )
 {
 ?>
-                <li class="hero js-hero js-hero-<?=slugify_name( $hero_name );?>" data-is-countered-by="<?=pipeify( heroes_strong_against( $data_ow2, $hero_id ) );?>" title="<?=$hero_name;?>">
-                    <img src="/assets/images/ow2/hero-<?=slugify_name( $hero_name );?>.png" loading="lazy" alt="<?=$hero_name;?>">
-                    <div class="name"><?=shorten_hero_name( $hero_name );?></div>
-                </li><!-- hero -->
+            <li class="hero js-hero js-hero-<?=slugify_name( $hero_name );?>" data-is-countered-by="<?=pipeify( heroes_strong_against( $data_ow2, $hero_id ) );?>" title="<?=$hero_name;?>">
+                <img src="/assets/images/ow2/hero-<?=slugify_name( $hero_name );?>.png" loading="lazy" alt="<?=$hero_name;?>">
+                <div class="name"><?=shorten_hero_name( $hero_name );?></div>
+            </li><!-- hero -->
 <?php
 }
 
 ?>
-            </ul><!-- heroes -->
+        </ul><!-- heroes -->
 
-        </div><!-- role -->
+    </div><!-- role -->
 
 <?php endforeach; // $roles ?>
 
